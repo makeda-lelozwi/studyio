5cd392d4c5c223165a97bef38f2bb21d3051f893
Lelo Dumeko <lelodumeko2@gmail.com>
2025-04-09 18:34:26 +0200

    2025.04.09UpdateCourses

--- client/src/app/api/course.tsx
+++ client/src/app/api/course.tsx
@@ -1,10 +1,11 @@
 "use client";
 import { BASE_URL } from "@/constant";
-import { getUserData } from "@/helper";
-import { CourseResponse } from "@/types";
+import { getToken, getUserData } from "@/helper";
+import { CourseEntry, CourseResponse } from "@/types";
 import qs from "qs";
 
 const userId = getUserData().user_id; //getting user info from Cookies
+const token = getToken();
 const queries = qs.stringify(
   userId
     ? {
@@ -36,6 +37,75 @@ export const getCourses = async (): Promise<CourseResponse | null> => {
     return data;
   } catch (error) {
     console.error("Fetch courses error: ", error);
+    return null;
+  }
+};
+
+//create new courses
+export const createNewCourse = async (
+  title: string,
+  description: string,
+  cover?: number | null,
+  price?: number | null
+) => {
+  try {
+    let data;
+    const response = await fetch(`${BASE_URL}/courses`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        data: { title, description, price, userId, cover },
+      }),
+    });
+
+    if (response.ok) {
+      data = await response.json();
+    } else {
+      data = null;
+      throw new Error("Course creation failed.");
+    }
+    return data;
+  } catch (error) {
+    console.error("Error creating course:", error);
+    return null;
+  }
+};
+
+export const updateCourse = async (
+  documentId: string,
+  title: string,
+  description: string,
+  cover?: number | null,
+  price?: number | null
+): Promise<CourseResponse | null> => {
+  const data: CourseEntry | null = {};
+  if (title != null) data.title = title;
+  if (description != null) data.description = description;
+  if (price != null) data.price = price;
+  if (userId != null) data.user_id = userId;
+  if (cover != null) data.cover = cover;
+
+  try {
+    const response = await fetch(`${BASE_URL}/courses/${documentId}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ data }),
+    });
+
+    if (response.ok) {
+      const dataResponse = await response.json();
+      return dataResponse;
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.error("Error updating course:", error);
     return null;
   }
 };
--- client/src/app/components/Course.tsx
+++ client/src/app/components/Course.tsx
@@ -1,14 +1,32 @@
 "use client";
+import UploadFileIcon from "@mui/icons-material/UploadFile";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import Fab from "@mui/material/Fab";
 import EditIcon from "@mui/icons-material/Edit";
 import { CourseProps } from "@/types";
+import {
+  Button,
+  CardActions,
+  Grid2,
+  IconButton,
+  TextField,
+} from "@mui/material";
+import { useAuthContext } from "../context/AuthContext";
+import { ChangeEvent, useState } from "react";
+import { updateCourse } from "../api/course";
+import { uploadToMediaGallery } from "../api/upload";
 
 const Course = ({ course }: CourseProps) => {
+  const user = useAuthContext().user;
+  const [edit, setEdit] = useState(false);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState<number | null>(null);
+  const [cover, setCover] = useState<number | null>(null);
+
   const imageUrl = course.cover?.url
     ? `http://localhost:1337${course.cover.url}`
     : "";
@@ -22,30 +40,146 @@ const Course = ({ course }: CourseProps) => {
       day: "numeric",
     });
   };
-  const formattedDate = course.publishedAt
-    ? formatDate(course.createdAt)
-    : "Unknown Date";
+
+  const updatedDate = formatDate(course.updatedAt);
+
+  const toggleEditCourse = () => {
+    setEdit(!edit);
+  };
+
+  const handleEditCourse = async (
+    title: string,
+    description: string,
+    price: number | null
+  ) => {
+    toggleEditCourse();
+    await updateCourse(course.documentId, title, description, cover, price);
+    location.reload(); // TODO: find a way to trigger dashboard refresh when courses are updated
+  };
+
+  const handleFileUpload = async (event: ChangeEvent<HTMLInputElement>) => {
+    const imageUpload = await uploadToMediaGallery(event);
+    if (imageUpload) {
+      const imageId = imageUpload[0].id;
+      setCover(imageId);
+    }
+  };
+
   return (
     <Card sx={{ margin: 2, width: 345 }}>
-      <CardHeader
-        title={course.title}
-        subheader={`Created on ${formattedDate}`}
+      <Grid2
+        container
+        sx={{ display: "flex", flexWrap: "wrap" }}
+        alignItems="center"
+        justifyContent="center"
       >
-        <Fab>
-          <EditIcon> </EditIcon>
-        </Fab>
-      </CardHeader>
-      {imageUrl ? (
-        <CardMedia component="img" height="194" image={imageUrl} alt="" />
+        <CardHeader
+          title={
+            !edit ? (
+              course.title
+            ) : (
+              <TextField
+                required
+                id="outlined-basic"
+                label="Course Title"
+                variant="outlined"
+                value={title}
+                sx={{ width: 200 }}
+                onChange={(e) => setTitle(e.target.value)}
+                autoComplete="off"
+              />
+            )
+          }
+          subheader={`Updated on ${updatedDate}`}
+        ></CardHeader>
+        {user && (
+          <IconButton aria-label="update course" onClick={toggleEditCourse}>
+            <EditIcon />
+          </IconButton>
+        )}{" "}
+      </Grid2>
+      {!edit ? (
+        imageUrl ? (
+          <CardMedia
+            component="img"
+            height="194"
+            image={imageUrl}
+            alt="Course cover image"
+          />
+        ) : (
+          <Typography>No Image Loaded</Typography>
+        )
       ) : (
-        <Typography>No Image Loaded</Typography>
+        <>
+          <Button
+            component="label"
+            variant="outlined"
+            startIcon={<UploadFileIcon />}
+            sx={{ width: 300, marginLeft: 2 }}
+          >
+            Upload Course Cover
+            <input
+              type="file"
+              hidden
+              onChange={handleFileUpload}
+              accept="image/*"
+            />
+          </Button>
+          {cover && <Typography>Image uploaded</Typography>}
+        </>
       )}
       <CardContent>
-        <Typography variant="body2" sx={{ color: "text.secondary" }}>
-          {course.price ? "R" + course.price : ""}
-        </Typography>
-        <Typography variant="body2">{course.description}</Typography>
+        {!edit ? (
+          <Typography variant="body2" sx={{ color: "text.secondary" }}>
+            {course.price ? "R" + course.price : ""}
+          </Typography>
+        ) : (
+          <TextField
+            id="outlined-basic"
+            type="number"
+            label="Course price"
+            variant="outlined"
+            value={price === null ? null : price}
+            sx={{ width: 150 }}
+            onChange={(e) => {
+              const value = e.target.value;
+              setPrice(value === null ? null : Math.abs(Number(value)));
+            }}
+            autoComplete="off"
+          />
+        )}
+        {!edit ? (
+          <Typography variant="body2">{course.description}</Typography>
+        ) : (
+          <TextField
+            required
+            id="outlined-basic"
+            label="Course description"
+            variant="outlined"
+            multiline
+            rows={4}
+            sx={{ width: 200, marginTop: 2 }}
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            autoComplete="off"
+          />
+        )}
       </CardContent>
+      {edit && (
+        <CardActions>
+          <Button
+            size="small"
+            type="submit"
+            color="primary"
+            onClick={() => handleEditCourse(title, description, price)}
+          >
+            Save
+          </Button>
+          <Button size="small" color="primary" onClick={toggleEditCourse}>
+            Cancel
+          </Button>
+        </CardActions>
+      )}
     </Card>
   );
 };
